989ba4d93fb35b3a0aad25638abaa18ba0218523
KAExtended <92701512+KAExtended@users.noreply.github.com>
2021-10-18 04:03:58 -0500

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,77 @@
+var sidebarContent = [
+   {
+      type: "cat",
+      name: "KHAN ACADEMY DOCS",
+      address: "/ka/ka-docs.html"
+   },
+   {
+      type: "subcat",
+      name: "KA Tips and Tricks",
+      address: "/ka/ka-tips-tricks.html"
+   },
+   {
+      type: "subcat",
+      name: "Processing.js Docs",
+      address: "/ka/pjs-docs.html"
+   },
+   {
+      type: "subcat",
+      name: "Web Docs",
+      address: "/ka/web-docs.html"
+   },
+   {
+      type: "subcat",
+      name: "SQL Docs",
+      address: "/ka/sql-docs.html"
+   },
+   {
+      type: "subcat",
+      name: "KA API Docs",
+      address: "/ka/api-docs.html"
+   },
+   {
+      type: "cat",
+      name: "FAQ",
+      address: "/faq.html"
+   },
+   {
+      type: "subcat",
+      name: "KA FAQ",
+      address: "/faq/ka.html"
+   },
+   {
+      type: "subcat",
+      name: "KAE FAQ",
+      address: "/faq/kau.html"
+   },
+   {
+      type: "cat",
+      name: "TUTORIALS",
+      address: "/tutorials.html"
+   },
+   {
+      type: "subcat",
+      name: "Processing.js",
+      address: "/tutorials/pjs.html"
+   },
+   {
+      type: "subcat",
+      name: "Webpages",
+      address: "/tutorials/web.html"
+   },
+   {
+      type: "subcat",
+      name: "SQL",
+      address: "/tutorials/sql.html"
+   },
+   {
+      type: "subcat",
+      name: "KA API",
+      address: "/tutorials/pjs.html"
+   },
+   {
+      type: "contact",
+      name: "CONTACT US",
+      address: "/contact.html"
+   },
+];
